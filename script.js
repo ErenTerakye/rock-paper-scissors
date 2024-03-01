51b4a197a928e5c47251d6e2ab2e1e23af41ddc5
ErenTerakye <erenterakye@gmail.com>
2024-03-01 21:21:15 +0300

    Add getComputerChoice function

---
+++ script.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+  let randNum = Math.floor(Math.random() * 3);
+  if (randNum === 0) {
+    return "Rock";
+  } else if (randNum === 1) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
