4ecf06cddd51c773f03186653ad5defbecadfbdb
ErenTerakye <erenterakye@gmail.com>
2024-03-02 20:28:18 +0300

    Add playRound function

--- script.js
+++ script.js
@@ -9,6 +9,20 @@ function getComputerChoice() {
   }
 }
 
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "Tie!"
+  } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    return "You Win! " + playerSelection + " beats " + computerSelection;
+  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    return "You Win! " + playerSelection + " beats " + computerSelection;
+  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    return "You Win! " + playerSelection + " beats " + computerSelection;
+  } else {
+    return "You Lose! " + computerSelection + " beats " + playerSelection;
+  }
+}
+
 function capitalizeFirstLetter(str) {
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
