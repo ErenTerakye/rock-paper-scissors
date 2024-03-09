112f7b4f54a1d18ccee90a6b707e968776bde98e
ErenTerakye <erenterakye@gmail.com>
2024-03-09 11:51:17 +0300

    Show current scores

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let playerSelection;
 let computerSelection;
+let playerScore = 0;
+let computerScore = 0;
 
-let menu = document.querySelector("#container");
+const menu = document.querySelector("#container");
+
 menu.addEventListener('click', (event) => {
   playerSelection = event.target.textContent;
   playRound(playerSelection, computerSelection)
@@ -21,18 +24,25 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
   computerSelection = getComputerChoice();
   let result = document.querySelector("#result");
+  let score = document.querySelector("#score");
 
   if (playerSelection === computerSelection) {
     result.textContent = "Tie!";
   } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
     result.textContent = "You Win! " + playerSelection + " beats " + computerSelection;
+    playerScore++;
   } else if (playerSelection === "Paper" && computerSelection === "Rock") {
     result.textContent = "You Win! " + playerSelection + " beats " + computerSelection;
+    playerScore++;
   } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
     result.textContent = "You Win! " + playerSelection + " beats " + computerSelection;
+    playerScore++;
   } else {
     result.textContent = "You Lose! " + computerSelection + " beats " + playerSelection;
+    computerScore++;
   }
+
+  score.textContent = `Your score: ${playerScore} Computers score: ${computerScore}`;
 }
 
 function capitalizeFirstLetter(str) {
